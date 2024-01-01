18a58b665a2ea1f4233c09d1d5ddc7e832a3adc1
MaksGorobets <gorobets542@gmail.com>
2024-01-01 21:58:05 +0200

    Initial Commit

---
+++ StorifyQR/StorifyQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorifyQRApp.swift
+//  StorifyQR
+//
+//  Created by Maks Winters on 01.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct StorifyQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
