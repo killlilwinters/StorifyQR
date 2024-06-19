f0b312edf08b1a26d20b66ce3689cbdf93c237e4
MaksGorobets <gorobets542@gmail.com>
2024-06-19 15:45:22 +0300

    Location name disappearing in ItemDetailView bug fix

--- StorifyQR/Views/Coordinate2DMapView/Coordinate2DMapView.swift
+++ StorifyQR/Views/Coordinate2DMapView/Coordinate2DMapView.swift
@@ -4,34 +4,48 @@
 //
 //  Created by Maks Winters on 07.01.2024.
 //
+// https://stackoverflow.com/a/60113557
+//
 
 import SwiftUI
 import MapKit
 
 struct Coordinate2DMapView: View {
     
-    @Bindable var viewModel: Coordinate2DMapViewModel
+    @State var locationName = "Unknown locaiton"
+    let locationGeocoder = LocationGeocoder()
+    
+    var rawLocation: CLLocationCoordinate2D
+    @State var mapRegionPosition: MapCameraPosition
     
     var body: some View {
         VStack {
             Text("Registered location:")
                 .font(.system(.headline))
                 .padding(.horizontal)
-            Map(position: $viewModel.mapRegionPosition) {
-                Marker("Item's location", coordinate: viewModel.rawLocation)
+            Map(position: $mapRegionPosition) {
+                Marker("Item's location", coordinate: rawLocation)
             }
             .frame(height: 200)
             .clipShape(RoundedRectangle(cornerRadius: 15))
             .allowsHitTesting(false)
-            LocationNameBar(locationName: viewModel.locationName)
-                .onAppear(perform: viewModel.getLocationName)
+            LocationNameBar(locationName: locationName)
+                .onAppear(perform: getLocationName)
         }
         .modifier(ContentPad())
         .padding(.horizontal)
     }
     
+    func getLocationName() {
+        locationGeocoder.getLocationName(rawLocation: rawLocation) { place in
+            self.locationName = place
+        }
+    }
+    
     init(coordinate2D: Coordinate2D) {
-        self.viewModel = Coordinate2DMapViewModel(coordinate2D: coordinate2D)
+        let clLocation = CLLocationCoordinate2D(latitude: coordinate2D.latitude, longitude: coordinate2D.longitude)
+        self.rawLocation = clLocation
+        self.mapRegionPosition = .region(MKCoordinateRegion(center: clLocation, span: MapDetails.defaultSpan))
     }
     
 }
