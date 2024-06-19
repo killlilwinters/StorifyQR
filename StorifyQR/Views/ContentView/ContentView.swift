204e18e33e7e1c69d6a6a2fac4709a2cdcb00fa2
MaksGorobets <gorobets542@gmail.com>
2024-06-19 14:07:58 +0300

    UI improvement

--- StorifyQR/Views/ContentView/ContentView.swift
+++ StorifyQR/Views/ContentView/ContentView.swift
@@ -80,7 +80,7 @@ struct ContentView: View {
                     viewModel.processImport(result: result)
                 }
                 .alert("Import \(viewModel.importItem?.name ?? "")?", isPresented: $viewModel.importingAlert) {
-                    Button("Cancel") { }
+                    Button("Cancel", role: .cancel) { }
                     Button("Save") {
                         viewModel.saveItem()
                     }
